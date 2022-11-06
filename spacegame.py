e8c902e93088c6f3f145c9c02f92b62766af7c69
TattooedBandit <vic1ory@tutanota.com>
2022-11-06 11:20:13 -0500

    Edit introduction

--- spacegame.py
+++ spacegame.py
@@ -1,19 +1,28 @@
 # Space Warriors Game
 print()
-print('Welcome to Space Warriors.')
-print()
-player_name = input('What is your name? ')
+player_name = input('Welcome to Space Warriors! Please enter your name. ')
 player_style = input('''
-Welcome ''' + player_name + '''! Space Warriors is a game that allows you to chose between a fighter spacecraft 
-or a defender spacecraft. Each spacecraft has it's own strengths and weaknesses. Please type in 
-"Fighter" or "Defender" ''')
+Welcome ''' + player_name + '''! Space Warriors is a game that allows you to chose between two classes of spacecraft. 
+The first selection is the fighter spacecraft. This ship does remarkable damage to it's enemies but is known to have a 
+weaker defense system. The second selection is the defender spacecraft. This ship does  lesser damage that the fight 
+ship but is known to have a strong defense system. Please choose which spacecraft you'd like to play by entering 
+"Fighter" or "Defender" ''').lower()
 print()
-if player_style == 'Fighter':
-    print('''You have selected the fighter style spacecraft. This spacecraft does remarkable damage 
-    to it\'s enemies but is known to have a weaker defense system.''')
+while player_style != 'fighter' and player_style != 'defender':
+    payer_style = input('Whoops, it looks like you did not chose "Fighter" or "Defender". Please try again ')
+if player_style == 'fighter':
+    input('''You have selected the fighter spacecraft. As a fighter your damage output is between a range of 6 to 10 
+damage and your hit points begin at 40.''')
 else:
-    print('''You have selected the defender style spacecraft. This spacecraft is known to have a strong
-defense system  but is know to do lesser damage to it\'s enemies.''')
+    input('''You have selected the defender spacecraft. As a defender your damage output is between a range of 3 and 7
+damage and your hit points begin at 50.''')
+
+
+
+
+
+
+
 
 
 
